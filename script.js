83cd25c24c5ed205e70702d768c12a3385a50aca
Bamson David <bamsondavid1@gmail.com>
2025-07-19 18:17:16 +0100

    Manually created 16 x 16 grid to know what exactly i am doing

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 const container = document.getElementById('container');
+container.classList.add("container");
+
+
+let userInput = prompt("Enter grid Dimension: ");
+
+
+
+for (let i = 0 ; i < userInput; i++) {
+    const div = document.createElement("div");
+    div.classList.add("box");
+    container.appendChild(div);
+}
